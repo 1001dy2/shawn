bcec716a09e04782f51d91dde53b88279c70803f
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2022-10-06 16:49:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+let x = 0
+let y = 0
+basic.forever(function () {
+    x = 0
+    y = 0
+    for (let index = 0; index < 4; index++) {
+        led.plot(x, y)
+        x += 1
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        led.plot(x, y)
+        y += 1
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        led.plot(x, y)
+        x += -1
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        led.plot(x, y)
+        y += -1
+        basic.pause(100)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+})
